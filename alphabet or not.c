b892c763411603a0f2ef0f576c7151abbb9e4122
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-21 08:41:24 -0700

    Create alphabet or not.c

---
+++ alphabet or not.c
@@ -0,0 +1,15 @@
+# Lavanya
+int main()
+{
+char ch;
+printf("enter an integer");
+scanf("%d",&ch);
+if{(ch>='a'&&ch,='z')!!(ch>="A"&&ch=='Z")]
+{printf("alphabet");
+}
+else
+{
+printf("not an alphabet");
+}
+return 0;
+}

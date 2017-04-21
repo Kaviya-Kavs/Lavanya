69c7c8b4c2f8cfdbd8b6c6b472059751976b016a
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-21 08:33:56 -0700

    Create vowel or consonant.c

---
+++ vowel or consonant.c
@@ -0,0 +1,19 @@
+# Lavanya
+int main()
+{
+char ch;
+int lowercasevowel,uppercasevowel;
+printf("enter a character");
+scanf("%d"'&ch);
+lowercasevowel=(ch==a!!ch==e!!ch==i!!ch==o!!ch==u);
+uppercasevowel=(ch==A!!ch==E!!ch==I!!ch==O!!ch==U);
+if(uppercasevowel!!lowercasevowel)
+{
+printf("%ch is a vowel",ch);
+}
+else
+{
+printf(%ch is a consonant",ch);
+}
+return 0;
+}

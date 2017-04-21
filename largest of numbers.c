ac44db1a093c51fe9cc1fb04b9572ef62f592f56
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-21 08:53:33 -0700

    Create largest of numbers.c

---
+++ largest of numbers.c
@@ -0,0 +1,20 @@
+# Lavanya
+int main()
+{
+double n1,n2,n3;
+printf("enter the numbers");
+scanf("%1f%1f%1f",&n1,&n2,&n3);
+if(n1>=n2&&n1>n3)
+{
+printf("%21f is largest";n1);
+}
+elseif(n2>n1&&n2>n3)
+{
+printf("%21f is largest";n2);
+}
+else
+{
+printf("%21f is largest";n3);
+}
+return o;
+}

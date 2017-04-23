017c9f615d1edd2665e8c24b96d56a58680cd276
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-22 23:53:29 -0700

    Create leap year or not.c

---
+++ leap year or not.c
@@ -0,0 +1,14 @@
+# Lavanya
+int main()
+{
+int year;
+printf("enter year");
+scanf("%d",&year);
+if(year%4==0)!!(year%100==0)!!(year%400==0){
+printf("%d is a leap year",);
+}
+else{
+printf("%d is not a leap year");
+}
+getch();
+}

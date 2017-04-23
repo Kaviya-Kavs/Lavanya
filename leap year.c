33ce1dcfb48bf2810f95b301e75faef18cca7410
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-23 00:02:18 -0700

    Create leap year.c

---
+++ leap year.c
@@ -0,0 +1,15 @@
+# Lavanya
+int main()
+{
+int year;
+printf("year is:");
+scanf(%d,&year);
+if(year%4==0)!!(year%100==0)!!(year%400==0)
+{
+printf("leap year");
+}
+else{
+printf("not a leap year");
+}
+getch();
+}

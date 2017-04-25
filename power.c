54473b7fa9063892fddf9fbace2bd75f6aa6b520
Kaviya-Kavs <mkaviyamohan96@gmail.com>
2017-04-24 23:22:44 -0700

    Create power.c

---
+++ power.c
@@ -0,0 +1,13 @@
+# Lavanya
+int main()
+{
+int base,exponent,counter,result=1;
+printf("enter base and exponent/n");
+scanf("%d%d",&base,&compenent);
+for(counter=0;counter<exponent;counter++)
+{
+result=result*base;
+}
+printf("%d^%d=%d",base,counter,result);
+return 0;
+}
